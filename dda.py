a044b700b1c5259cb511fdb776c525ec1b24add5
nashvincent <nashvincent32@gmail.com>
2019-08-12 13:37:26 +0530

    DDA import file

---
+++ dda.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import glBegin, glVertex2i, glEnd, glFlush, GL_POINTS
+
+def setPixel(xcoordinate,ycoordinate):
+    glBegin(GL_POINTS)
+    glVertex2i(xcoordinate,ycoordinate)
+    glEnd()
+    glFlush()
+
+def lineDDA(x0,y0,x1,y1):
+    dx = abs(x1-x0)
+    dy = abs(y1-y0)
+    x, y = x0, y0
+    steps = dx if dx > dy else dy
+    
+    xIncrement = dx/float(steps)
+    yIncrement = dy/float(steps)
+    setPixel(int(round(x)), int(round(y)))
+
+    for k in range(steps):
+        x+= xIncrement
+        y+= yIncrement
+        setPixel(int(round(x)), int(round(y)))  
